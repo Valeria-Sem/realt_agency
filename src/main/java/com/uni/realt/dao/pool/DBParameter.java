5da9515a893697cdb50ccb5de6b6e596b00b92de
Valeria-Sem <lerasemenenya@gmail.com>
2021-12-29 02:59:31 +0300

    create connection pool

---
+++ src/main/java/com/uni/realt/dao/pool/DBParameter.java
@@ -0,0 +1,11 @@
+package com.uni.realt.dao.pool;
+
+public class DBParameter {
+    public static final String DB_DRIVER ="dbDriver";
+    public static final String DB_URL ="dbUrl";
+    public static final String DB_USER ="dbUser";
+    public static final String DB_PASSWORD ="dbPassword";
+    public static final String DB_POOL_SIZE ="dbPoolSize";
+
+    private DBParameter(){}
+}
---
+++ src/main/java/com/uni/realt/dao/pool/DBResourceManager.java
@@ -0,0 +1,18 @@
+package com.uni.realt.dao.pool;
+
+import java.util.ResourceBundle;
+
+public class DBResourceManager {
+    private static final DBResourceManager instance = new DBResourceManager();
+    private static final String database = "database";
+
+    private final ResourceBundle bundle = ResourceBundle.getBundle(database);
+
+    public static DBResourceManager getInstance() {
+        return instance;
+    }
+
+    public String getValue(String key){
+        return bundle.getString(key);
+    }
+}
